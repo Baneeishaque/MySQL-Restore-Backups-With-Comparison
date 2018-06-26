a9dc19c8d889d831fff995e913d1161da7d432be
baneeishaque <baneeishaque@gmail.com>
2018-06-26 20:51:51 +0530

    ok

--- mysql_resote_sql_gz_from_folder.bat
+++ mysql_resote_sql_gz_from_folder.bat
@@ -11,13 +11,9 @@ ECHO Mode is %mode%
 
 SET folder=%folder:"=%
 ECHO Folder is %folder%
-REM DIR %folder%
-REM DIR %folder% /S /B /A:-D *.sql.gz 2>NUL
 DIR %folder% /S /B /O-S | findstr /E .sql.gz 2>NUL > backups.list
-REM GOTO :end
 
 SET db_prefix=%~2
-REM ECHO DB Prefix : %2
 IF "%db_prefix%"=="" (
 	SET /P db_prefix=Please Enter DB Prefix : 
 )
@@ -36,48 +32,47 @@ GOTO end
 :do_line
 SET line=%1
 REM ECHO Line %index% is %line%
-ECHO create database `%db_prefix%%index%` ^| mysql -uroot
-REM ECHO create database `%db_prefix%%index%` | mysql -uroot
-ECHO gunzip ^< %line% ^| mysql -uroot %db_prefix%%index%
-REM gunzip < %line% | mysql -uroot %db_prefix%%index%
+REM ECHO create database `%db_prefix%%index%` ^| mysql -uroot
+ECHO create database `%db_prefix%%index%` | mysql -uroot 1>NUL
+REM ECHO gunzip ^< %line% ^| mysql -uroot %db_prefix%%index%
+gunzip < %line% | mysql -uroot %db_prefix%%index% 1>NUL
 
 call set line=%%line:!find!=!replace!%%
 FOR %%i IN ("%line%") DO (
-	SET backup_file_name=%%~ni
+	SET backup_file_name=%%~ni.gz
 	ECHO !backup_file_name! >> backup_files.list
-	REM PAUSE
 )
 
 ECHO %line% - %db_prefix%%index% >> map.txt
 ECHO %line% to %db_prefix%%index%
-REM ECHO %line%
 
 IF NOT %index% EQU 0 (
 
 	ECHO Project File : !backup_file_name!_to_!previous_db!.mdc
 	DEL ^"!backup_file_name!_to_!previous_db!.mdc^" 2>NUL
-	COPY redgate_mdc_project.template ^"!backup_file_name!_to_!previous_db!.mdc^"
+	COPY redgate_mdc_project.template /Y ^"!backup_file_name!_to_!previous_db!.mdc^" 1>NUL
 
-	set InputFile=^"!backup_file_name!_to_!previous_db!.mdc^"
-	set OutputFile=^"!backup_file_name!_to_!previous_db!.mdc_new^"
-	set "_strFind=    <name>project_name</name>"
-	set "_strInsert=    <name>!backup_file_name!_to_!previous_db!</name>"
+	SET InputFile=!backup_file_name!_to_!previous_db!.mdc
+	SET OutputFile=!backup_file_name!_to_!previous_db!.mdc_new
+	REM ECHO Input File : !InputFile!
+	REM ECHO Output File : !OutputFile!
 
+	SET "_strFind=    <name>project_name</name>"
+	SET "_strInsert=    <name>!backup_file_name!_to_!previous_db!</name>"
 
-	>%OutputFile% (
-  		for /f "usebackq delims=" %%A in (%InputFile%) do (
-    		REM if "%%A" equ "%_strFind%" (echo %_strInsert%) else (echo %%A)
-  		)
+	> "!OutputFile!" (
+		for /f "usebackq delims=" %%A in ("!InputFile!") DO (
+			if "%%A" EQU "!_strFind!" ( ECHO !_strInsert! ) else ( ECHO %%A )
+		)
 	)
 
-	REM del ^"!backup_file_name!_to_!previous_db!.mdc^"
-	REM rename ^"!backup_file_name!_to_!previous_db!.mdc_new^" ^"!backup_file_name!_to_!previous_db!.mdc^"
+	DEL ^"!backup_file_name!_to_!previous_db!.mdc^"
+	RENAME ^"!backup_file_name!_to_!previous_db!.mdc_new^" ^"!backup_file_name!_to_!previous_db!.mdc^"
 
 )
 
 SET previous_db=!backup_file_name!
 SET /A index+=1
-
-PAUSE
+REM PAUSE
 
 :end
